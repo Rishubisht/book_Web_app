642b8c454f1bbb56e15ba1925ab9cb309d30198a
priyansh <priyanshbisht01@bmail.biz>
2024-08-26 18:07:38 +0530

    second commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -42,6 +42,7 @@ const Footer = () => {
               consectetur adipisicing elit. Possimus, voluptate.{" "}
             </p>
             <br />
+            {/* comment */}
             <div className="flex items-center gap-3">
               <FaLocationArrow />
               <p>Noida, Uttar Pradesh</p>
